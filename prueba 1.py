4f0ce536f55e0a050414a6c754dd53b10fe7e96d
sguillenuoc <84874540+sguillenuoc@users.noreply.github.com>
2022-03-31 19:31:49 +0200

    Update prueba 1.py

--- prueba 1.py
+++ prueba 1.py
@@ -1,53 +1,71 @@
+from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import requests
-from bs4 import BeautifullSoup
+from time import sleep
+import os
+
+
+def get_robot_txt(url):
+    if url.endswith('/'):
+        path = url
+    else:
+        path = url + '/'
+    req = requests.get(path + "robots.txt", data=None)
+    return req.text
+
 
 # web a analizar
 
 url = "https://geoportalgasolineras.es/#/Inicio"
 
+# Setting options for the webdriver
+
+option = webdriver.ChromeOptions()
+option.add_argument("--incognito")  # open incognito mode
+option.add_argument("user-agent=AcademicCrawler")
+
+# set our UserAgent name, in this case AcademicCrawler
+
+# Getting current folder path
+My_path = os.path.dirname(os.path.abspath(__file__))
+
 # mirar codigo html del formulario para encontrar campos minimos a rellenar
 # parametros formulario
 
 StationsMenu = "Estaciones servicio"
-ccaa = "BARCELONA"
+ccaa = "CATALUNYA"
 combustible = "Gasolina 95 E5"
 
 # inicio navegador
 
-nav = werbdriver.Edge(executable_path='C:\Program Files (x86)\Microsoft\Edge\Application\msedgedriver.exe')
-nav.get(url)
+browser = webdriver.Chrome(executable_path=My_path + '/chromedriver.exe', chrome_options=option)
+browser.get(url)
 
 # introducir todos los campos mínimos
 # introducir parametro station en formulario
 
-stf = driver.find_element_by_name("tipoBusqueda")
+stf = browser.find_element(By.NAME, "tipoBusqueda")
 stf.send_keys(StationsMenu)
 sleep(5)
 
-#introducir parametro ccaa en formulario
+# introducir parametro ccaa en formulario
 
-caf = driver.find_element_by_name ("provincia")
-caf.send_keys (ccaa)
+caf = browser.find_element(By.NAME, "provincia")
+caf.send_keys(ccaa)
 sleep(5)
 
-carf = driver.find_element_by_name ("tipoCarburante")
-caf.send_keys (combustible)
+carf = browser.find_element(By.NAME, "tipoCarburante")
+caf.send_keys(combustible)
 sleep(5)
-
 
 # pulsar boton buscar (mirar etiqueta de boton)
 
-search = driver.find_element_by_id("botonBuscar")
-submit.click()
+search = browser.find_element(By.ID, "botonBuscar")
+search.click()
 
 # inicio scraping
 
 result = requests.get(url)
 src = result.content
-soup = BeautifulSoup(src, "xlml)
-                     
-fs = bs.find_all("fieldset")
-for t in fs:
-    print(t.get_text())
-
+soup = BeautifulSoup(src, "xlml")
